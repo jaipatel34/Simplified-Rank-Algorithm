ccd154d61b88a4837226e05a4f06bec9f20f57c1
jaipatel34 <130802012+jaipatel34@users.noreply.github.com>
2024-12-22 10:59:44 -0500

    Update AdjacencyList.h

--- src/AdjacencyList.h
+++ src/AdjacencyList.h
@@ -1,15 +1,26 @@
-#pragma once
 
+//header file
+#pragma once
+#include <iostream>
 #include <string>
+#include <map>
+#include <vector>
+#include <unordered_map>
+
 
 using namespace std;
 
 class AdjacencyList {
-    private:
-    //Think about what member variables you need to initialize
-    public:
-    //Think about what helper functions you will need in the algorithm
+private:
+    unordered_map<string, double> rank;
+    unordered_map<string, vector<string>> graph;
+    unordered_map<string, int> out_deg;
+
+public:
+    AdjacencyList();
+    void addEdge(const string& fromEdge, const string& toEdge);
+    void GetRank();
     string PageRank(int n);
+    ~AdjacencyList();
 };
 
-// This class and method are optional.
