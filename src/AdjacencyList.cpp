7f4ffb8a13e982b429782bc35550eedadd989de4
jaipatel34 <130802012+jaipatel34@users.noreply.github.com>
2024-12-22 11:00:03 -0500

    Update AdjacencyList.cpp

--- src/AdjacencyList.cpp
+++ src/AdjacencyList.cpp
@@ -1,16 +1,98 @@
+
 #include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <cmath>
+#include <iomanip>
+#include <sstream>
 #include "AdjacencyList.h"
 
 using namespace std;
 
-// prints the PageRank of all pages after p powerIterations in ascending
-// alphabetical order of webpages and rounding rank to two decimal places
-string AdjacencyList::PageRank(int n){
-    // optionally, store your output in a string/stringstream and then return it from this function after printing so that it is easier to test with Catch
-    string result;
+//Constructor
+AdjacencyList::AdjacencyList(){
 
-    // do your page rank
-
-    cout << result;
-    return result;
 }
+
+//finds the edge and adds it
+void AdjacencyList::addEdge(const string& fromEdge, const string& toEdge) {
+    if (graph.find(toEdge) == graph.end()) {
+        graph[toEdge] = {};
+    }
+    if (graph.find(fromEdge) == graph.end()) {
+        graph[fromEdge] = {};
+    }
+
+    graph[fromEdge].push_back(toEdge);
+    out_deg[fromEdge]++;
+};
+
+//finds the rank
+void AdjacencyList::GetRank() {
+    double firstr = 1.0 / graph.size();
+    for (const auto& ThePage : graph) {
+        rank[ThePage.first] = firstr;
+    }
+};
+
+//finds the PageRank
+string AdjacencyList::PageRank(int n) {
+    GetRank();
+
+    if (n == 1) {
+        stringstream newAnswer;
+
+        //convert for sorting
+        vector<pair<string, double>> FirstRank(rank.begin(), rank.end());
+        sort(FirstRank.begin(), FirstRank.end());
+
+        //get output
+        for (const auto& ThePage : FirstRank) {
+            newAnswer << ThePage.first << " " << fixed << setprecision(2) << ThePage.second<< "\n";
+        }
+
+        string answer = newAnswer.str();
+        cout << answer;
+        return answer;
+    }
+
+    //find the PageRank based of times it goes through
+    for (int it = 1; it < n; it++) {
+        unordered_map<string, double> newRank;
+
+        for (const auto& ThePage: graph) {
+            newRank[ThePage.first] = 0.0;
+        }
+
+        for (const auto& ThePage:graph) {
+            double div = rank[ThePage.first] / out_deg[ThePage.first];
+
+            for (const auto& ThePages: ThePage.second) {
+                newRank[ThePages] += div;
+            }
+        }
+
+        rank = newRank;
+    }
+    stringstream NewAnswer;
+
+    //sort url
+    vector<pair<string, double>> sorte(rank.begin(), rank.end());
+    sort(sorte.begin(), sorte.end());
+
+    //for output
+    for (const auto& ThePage : sorte) {
+        NewAnswer << ThePage.first << " " << fixed << setprecision(2) << ThePage.second << "\n";
+    }
+
+    string answers = NewAnswer.str();
+    cout << answers;
+    return answers;
+};
+
+//Destructor
+AdjacencyList::~AdjacencyList() {
+
+};
